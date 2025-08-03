d0bdb4a5b9f465910b0e38152696b85db529ec0b
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-08-03 16:17:44 +0530

    cors enable

--- server.js
+++ server.js
@@ -8,10 +8,25 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5174",
+// app.use(cors({
+//     origin: "http://localhost:5174",
+//     credentials: true,
+//   }));
+
+app.use(
+  cors({
+    // origin: ["https://taskmanager-frontend-mpcvr9lwv-arkadeep-bags-projects.vercel.app","https://taskmanager-frontend-ten.vercel.app", "http://localhost:3000", "http://localhost:3001","http://localhost:5173"],
+    origin: function (origin, callback) {
+      if (!origin || origin.includes("vercel.app")) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
-  }));
+  })
+);
 
 app.use(express.json());
 
